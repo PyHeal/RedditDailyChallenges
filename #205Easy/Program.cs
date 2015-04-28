f721682cf9b51b4331011c829c0852ce69053345
Tyler Woodward <misterhectorheal@gmail.com>
2015-04-28 03:17:42 -0400

    Basic date ranges finished
    Haven't gotten any of the different cases yet, or the edge cases. Feel
    like the implementation I was planning on doing isn't that good so I've
    stepped back now and just been thinking about it. I'll work on it more
    tomorrow!

--- #205Easy/Program.cs
+++ #205Easy/Program.cs
@@ -15,7 +15,7 @@ namespace FriendlyDateRanges
         public string strMonth;
         public string dateSuff;
 
-        public Date(int day, int month, int year)
+        public Date(int year, int month, int day)
         {
             this.day = day;
             this.month = month;
@@ -51,10 +51,59 @@ namespace FriendlyDateRanges
                 this.dateSuff = "th";
             }
         }
+
+        public int yearCompare (Date other)
+        {
+            int yearDistance = other.year - this.year;
+
+            if( yearDistance == 1)
+            {
+                if (other.month > this.month) //It's been a year
+                {
+                    return 1;
+                }
+                else if (other.month < this.month) //It hasn't been a year
+                {
+                    return 0;
+                }
+                else if (other.month == this.month) //It might have been a year
+                {
+                    if(other.day < this.day) //Not a year
+                    {
+                        return 0;
+                    }
+                    else if (other.day > this.day) //It's been a year
+                    {
+                        return 1;
+                    }
+                    else //They must be the same day, so it's been a year exactly 
+                    {
+                        return -1;
+                    }
+                }
+            }
+
+            return other.year - this.year;
+        }
     }
 
     class Program
     {
+        int yearDiffernece;
+        int currentYear;
+
+        void Program(int yd, int cy)
+        {
+            this.yearDiffernece = yd;
+            this.currentYear = cy;
+        }
+
+        void printDateRange()
+        {
+
+        }
+
+
         static int Main(string[] args)
         {
             if(args.Length != 2)
@@ -63,19 +112,33 @@ namespace FriendlyDateRanges
                 return 1;
             }
 
+            string[] dateOne = args[0].Split('-');
+/*            foreach (string s in what)
+            {
+                Console.WriteLine(s);
+            }*/
 
+            string[] dateTwo = args[1].Split('-');
+ /*           foreach (string s in otherWhat)
+            {
+                Console.WriteLine(s);
+            }*/
 
+            Date one = new Date(Convert.ToInt32(dateOne[0]), Convert.ToInt32(dateOne[1]), Convert.ToInt32(dateOne[2]));
+            Date two = new Date(Convert.ToInt32(dateTwo[0]), Convert.ToInt32(dateTwo[1]), Convert.ToInt32(dateTwo[2]));
 
-            //Date one = new Date();
-            //Date two = new Date();
+            one.findMonth();
+            one.dateCrap();
 
+            two.findMonth();
+            two.dateCrap();
 
+            Console.Write(one.strMonth + " " + one.day + one.dateSuff + ", " + one.year + " - ");
+            Console.WriteLine(two.strMonth + " " + two.day + two.dateSuff + ", " + two.year);
 
+            int catchMe = one.yearCompare(two);
 
-
-
-
-
+            Console.WriteLine(catchMe);
 
             return 0;
         }
