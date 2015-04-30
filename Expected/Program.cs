d26fdb323ad87c82eb2ff95f7aaded45eee6a3de
Tyler Woodward <misterhectorheal@gmail.com>
2015-04-29 22:07:47 -0400

    Inital Commit for Expected
    Gonna be a program I will use in conjuction with a batch script for
    these challenges. So I can just quickly compare if the outputs are the
    same.
    
    Not finished yet, just learning File IO in C# right now

---
+++ Expected/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expected
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            System.IO.StreamReader output = new System.IO.StreamReader(@"C:\Users\tyler\Documents\Visual Studio 2013\Projects\Expected\Expected\output.txt");
+            System.IO.StreamReader expected = new System.IO.StreamReader(@"C:\Users\tyler\Documents\Visual Studio 2013\Projects\Expected\Expected\expected.txt");
+            while((line= output.ReadLine()) !=null)
+            {
+                Console.WriteLine(line);                
+            }
+
+            while((line =  expected.ReadLine()) !=null)
+            {
+                Console.WriteLine(line);                
+            }
+
+            output.Close();
+            expected.Close();
+            System.Console.ReadLine();
+
+
+        }
+    }
+}
