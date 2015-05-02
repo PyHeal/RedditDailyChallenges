121dab763ff770e4cb9b0c35a27b8b37ef699a91
Tyler Woodward <misterhectorheal@gmail.com>
2015-05-01 23:35:11 -0400

    Finished comparsion by line
    Comparsion by line works, in theory. Haven't complied the code or ran it
    yet but it works in theory.
    
    Also added some simple command args stuff, gotta figure out how to open
    files using the directory the exe is in plus the file name given.

--- Expected/Program.cs
+++ Expected/Program.cs
@@ -8,11 +8,45 @@ namespace Expected
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            
+
+            if(args.Length != 2)
+            {
+                Console.WriteLine("Format is Expected <ouput> <expected>");
+                return 1;
+            }
+
             string line;
+            string outputCon = args[0];
+            string expectedCon = args[1];
+
+            int counter = 0;
+            //TODO How do I use these strings to open the files?
+
             System.IO.StreamReader output = new System.IO.StreamReader(@"C:\Users\tyler\Documents\Visual Studio 2013\Projects\Expected\Expected\output.txt");
             System.IO.StreamReader expected = new System.IO.StreamReader(@"C:\Users\tyler\Documents\Visual Studio 2013\Projects\Expected\Expected\expected.txt");
+
+            for (int i = 0; i < output.Length; i++ )
+            {
+                if(output[i].Equals(expected[i]))
+                {
+                    Console.WriteLine("SAME");
+                }
+                else
+                {
+                    Console.WriteLine("Different");
+                    counter++;
+                }
+            }
+            if(counter != 0)
+            {
+                Console.WriteLine(counter + " Number of lines wrong");
+            }
+
+
+
             while((line= output.ReadLine()) !=null)
             {
                 Console.WriteLine(line);                
@@ -27,7 +61,7 @@ namespace Expected
             expected.Close();
             System.Console.ReadLine();
 
-
+            return 0;
         }
     }
 }
