ff25aa7590ea208e4adf896102c2ac1acc7d2320
Tyler Woodward <misterhectorheal@gmail.com>
2015-05-04 22:33:27 -0400

    Challenge #197
    Almost finished Challenge 197, the program can figure out if a entered
    number via command line is a vaild ISBN. Just need to clean up the
    output.
    Not sure if I will make a generator.

---
+++ #197Easy/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISBN_Validator
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if( args.Length != 1)
+            {
+                Console.WriteLine("Not Correct");
+                return 1;
+            }
+
+            int length = args[0].Length;
+            Console.WriteLine("The Length is " + length);
+            int otherLength =args[0].Length-1;
+            string ISBN = args[0];
+            //int ISBN = Convert.ToInt32(args[0]);
+            int[] test = new int[ISBN.Length];
+
+            for (int i = 0; i < args[0].Length; i++ )
+            {
+                Console.WriteLine("Substring " + i + " is " + ISBN.Substring(i, 1));
+                Console.WriteLine(length);
+                test[i] = Convert.ToInt32(ISBN.Substring(i, 1)) * length;
+                Console.WriteLine("Test "+ i + " is " + test[i]);
+                length = length - 1;
+            }
+
+
+           int sum = test.Sum();
+           Console.WriteLine("The sum is " + sum);
+           Console.WriteLine("The sum divided by 11 is "+ sum/11.0);
+                
+            
+            return 0;
+        }
+    }
+}
---
+++ #197Easy/README.md
@@ -0,0 +1,11 @@
+# RedditDailyChallenges
+https://www.reddit.com/r/dailyprogrammer/comments/2s7ezp/20150112_challenge_197_easy_isbn_validator/
+
+This program will vailidate an ISBN entered via commandline
+
+>Program 0-7475-3269-9 
+
+Vaild ISBN
+
+>Program
+<Vaild ISBN>
