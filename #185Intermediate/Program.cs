1d0e376c5a866dad063960c99979616af2f9163e
Tyler Woodward <misterhectorheal@gmail.com>
2015-05-06 22:37:04 -0400

    Started #185 Syntax Highlighting
    Just testing file I, and writing coloured font to the console. Wasted my
    time looking at how to generate HTML then sidetracked by ASP.NET...
    Hopefully get more done tomorrow.

---
+++ #185Intermediate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyntaxHighlighting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            System.IO.StreamReader source = new System.IO.StreamReader(@"C:\Users\tyler\Documents\Visual Studio 2013\Projects\SyntaxHighlighting\SyntaxHighlighting\test.py");
+
+
+            string test = source.ReadLine();
+
+            string[] what = new string[5];
+
+            what = test.Split(' ');
+
+            if(what[0].Equals("print"))
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write("print ");
+                Console.ResetColor();
+            }
+            Console.WriteLine(what[1]);
+
+
+        }
+    }
+}
---
+++ #185Intermediate/README.md
@@ -0,0 +1,9 @@
+# RedditDailyChallenges
+
+Pick any program language and write a converter for it, which will convert source code of the language to a highlighted format.
+
+Currently uses .NET, Console.ForegroundColor. When I'm finished I want to export an HTML file with the highlights.
+
+Currently picked Python but I might change to another language
+
+https://www.reddit.com/r/dailyprogrammer/comments/2k2zdv/10232014_challenge_185_intermediate_syntax/
